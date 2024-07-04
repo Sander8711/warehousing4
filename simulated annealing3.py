9dcab910034201efe54ae518c445cb1a3120202d
Sander8711 <150798885+Sander8711@users.noreply.github.com>
2024-07-04 20:43:08 +0200

    sdf

--- simulated annealing3.py
+++ simulated annealing3.py
@@ -5,6 +5,8 @@ import prp.utils as utils
 import json
 import numpy as np
 import prp.core.costs as costs_mod
+import random
+import copy
 
 #region set directories
 LAYOUT_FILE = "data/10-layout.json"
@@ -35,15 +37,16 @@ warehouse = load_problem()
 
 #select solver
 solver = CheapestPlaceSolver(warehouse, costs_type=CostsType.DECISION)
+solver2 = RandomSolver(warehouse)
 
 #initialize arrays
 iterations = 1000
 x=0
 solution = []
-costs = np.zeros(iterations,dtype=int)
+costs = np.empty(iterations,dtype=int)
 pod_location=[1,2,3,4,5,6,7,8,9,10]
-Original_Configuration=np.zeros((iterations,10),dtype=int)
-Next_Configuration = np.zeros((iterations,10),dtype=int)
+Original_Configuration=np.empty((iterations,10),dtype=int)
+Next_Configuration = np.empty((iterations,10),dtype=int)
 Original_Configuration[x] = [1,2,3,4,5,6,7,8,9,10]
 
 #region initial solution with cheapest place    
@@ -53,7 +56,7 @@ while not warehouse.finished():
     
    #store movements in arrays in order to use in heuristic
     retrieved_pod = warehouse.departure_generator.departures[0][0]
-    previous_location=pod_location[retrieved_pod - 1]
+    previous_location = pod_location[retrieved_pod - 1] 
     Next_Configuration[x]=Original_Configuration[x]
     Next_Configuration[x][previous_location-1]=0
     Next_Configuration[x][place_id-1]=pod
@@ -70,6 +73,62 @@ while not warehouse.finished():
     warehouse.next(place_id)
 #endregion
 
+def generateneighborsolution(solution):
+    warehouse2 = copy.deepcopy(warehouse)  # Create a deep copy of warehouse
+    degree_of_destruction = 10
+    randomindex = np.random.randint(len(solution))
+    newsolution = solution[:randomindex] + solution[randomindex + degree_of_destruction:]
+    x = randomindex 
+
+    while len(newsolution) < iterations: 
+        place_id = np.random.randint(1,11)
+        pod = np.random.randint(1,11)
+        station_id = np.random.randint(1,2)
+            
+        print(place_id, pod, station_id)
+        newsolution.append(place_id)
+
+    new_cost = sum(costs)  # Sum the costs to get the new cost
+    return newsolution, new_cost
+
+
+
+'''
+#Simulated annealing parameters
+initial_temperature = 1000
+cooling_rate = 0.995
+markov_chain_length = 100
+min_temp = 1
+current_temp = initial_temperature
+
+#Simulated annealing algorithm
+while current_temp > min_temp:
+
+    for i in range(markov_chain_length):
+
+        if neighborsolution < currentsolution:
+
+            if neighborsolution < bestsolutionsofar:
+                storebestsolution
+                bestsolutionsofar = neighborsolution
+            else:
+                currentsolution = neighborsolution
+        
+        else:
+            r = np.random()
+            if r < np.exp((currentsolution - neighborsolution) / current_temp):
+                currentsolution = neighborsolution
+            else:
+                restorebestsolution
+
+    current_temp = current_temp * cooling_rate                
+'''
+
+newsolution, newcost = generateneighborsolution(solution)
+print(solution)
+print(newcost)
+print(newsolution)
+ 
 #region print
 print("Total costs: {} at time {}.".format(warehouse.total_costs, warehouse.t))
 print(np.sum(costs))
@@ -78,4 +137,3 @@ utils.create_missing_directories_of_file(SOLUTION_FILE)
 with open(SOLUTION_FILE, 'w') as outfile:
     recorder.store_solution_to_json(solution, outfile)
 #endregion
-    
