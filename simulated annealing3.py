32b7cd538b7334b5dc4d964f1a9212c44abcdb59
Sander8711 <150798885+Sander8711@users.noreply.github.com>
2024-07-05 11:20:57 +0200

    sdf

--- simulated annealing3.py
+++ simulated annealing3.py
@@ -7,6 +7,7 @@ import numpy as np
 import prp.core.costs as costs_mod
 import random
 import copy
+from tqdm import tqdm
 
 #region set directories
 LAYOUT_FILE = "data/10-layout.json"
@@ -36,17 +37,17 @@ warehouse = load_problem()
 #endregion
 
 #select solver
-solver = CheapestPlaceSolver(warehouse, costs_type=CostsType.DECISION)
-solver2 = RandomSolver(warehouse)
+solver2 = CheapestPlaceSolver(warehouse, costs_type=CostsType.DECISION)
+solver = RandomSolver(warehouse)
 
 #initialize arrays
 iterations = 1000
 x=0
 solution = []
-costs = np.empty(iterations,dtype=int)
+costs = np.zeros(iterations,dtype=int)
 pod_location=[1,2,3,4,5,6,7,8,9,10]
-Original_Configuration=np.empty((iterations,10),dtype=int)
-Next_Configuration = np.empty((iterations,10),dtype=int)
+Original_Configuration=np.zeros((iterations,10),dtype=int)
+Next_Configuration = np.zeros((iterations,10),dtype=int)
 Original_Configuration[x] = [1,2,3,4,5,6,7,8,9,10]
 
 #region initial solution with cheapest place    
@@ -74,66 +75,78 @@ while not warehouse.finished():
 #endregion
 
 def generateneighborsolution(solution):
-    warehouse2 = copy.deepcopy(warehouse)  # Create a deep copy of warehouse
-    degree_of_destruction = 10
+    degree_of_destruction = 10                                                              #Initialize arrays
     randomindex = np.random.randint(len(solution))
-    newsolution = solution[:randomindex] + solution[randomindex + degree_of_destruction:]
-    x = randomindex 
+    newsolution = solution[:randomindex] + solution[randomindex + degree_of_destruction:]   #Destroy solution
+    newcost = list(costs[:randomindex]) + list(costs[randomindex + degree_of_destruction:])
+    x = len(newsolution)
 
-    while len(newsolution) < iterations: 
+    while len(newsolution) < iterations:                                                    #Loop until solution is repaired
         place_id = np.random.randint(1,11)
         pod = np.random.randint(1,11)
         station_id = np.random.randint(1,2)
-            
-        print(place_id, pod, station_id)
-        newsolution.append(place_id)
 
-    new_cost = sum(costs)  # Sum the costs to get the new cost
-    return newsolution, new_cost
+        if len(newcost) <= x:                                                               #Extend newcost list
+            newcost.append(0)
 
+        newsolution.append(place_id)                                                        #Add new place_id to solution list
+        newcost[x]= warehouse.costs.from_station(station_id, place_id)+ warehouse.costs.to_station(previous_location, station_id)
+        x += 1
 
+    total_new_cost = sum(newcost) 
 
-'''
+    return newsolution, total_new_cost                                                      #Return neighbor solution and cost
+
 #Simulated annealing parameters
-initial_temperature = 1000
+initial_temperature = 1000                                                                  #Initialize parameters
 cooling_rate = 0.995
 markov_chain_length = 100
 min_temp = 1
-current_temp = initial_temperature
+
+current_temp = initial_temperature                                                          #Copy starting solution and cost
+current_solution = solution.copy()
+current_cost = sum(costs)
+bestcostsofar = current_cost
+bestsolutionsofar = solution.copy()
+
+progress_bar = tqdm(total=1400, desc="Processing", unit="iteration")                        #Initialize progress bar
+iteration = 0
 
 #Simulated annealing algorithm
-while current_temp > min_temp:
-
+while current_temp > min_temp:                                                              #Loop until minimum temp is reached
     for i in range(markov_chain_length):
-
-        if neighborsolution < currentsolution:
-
-            if neighborsolution < bestsolutionsofar:
-                storebestsolution
-                bestsolutionsofar = neighborsolution
+        neighborsolution, neighborcost = generateneighborsolution(current_solution)         #Generate neighbor solution
+        if neighborcost < current_cost:
+            if neighborcost < bestcostsofar:                                                #Check if neighbor solution is better then current solution
+                bestsolutionsofar = neighborsolution.copy()
+                bestcostsofar = neighborcost
             else:
-                currentsolution = neighborsolution
-        
+                currentsolution = neighborsolution.copy()
+                current_cost = neighborcost      
         else:
-            r = np.random()
-            if r < np.exp((currentsolution - neighborsolution) / current_temp):
-                currentsolution = neighborsolution
+            r = np.random.rand()
+            if r < np.exp((current_cost - neighborcost) / current_temp):                    #Maybe accept even if solution is worse
+                currentsolution = neighborsolution.copy()
+                current_cost = neighborcost
             else:
-                restorebestsolution
+                current_solution = bestsolutionsofar.copy()                                 #Restore best solution this far if neighbor was not accepted
+                current_cost = bestcostsofar            
+    current_temp = current_temp * cooling_rate                                              #Update temperature with cooling rate       
+    progress_bar.update(1)
+    iteration += 1
 
-    current_temp = current_temp * cooling_rate                
-'''
+resultsolution = bestsolutionsofar.copy()                                                   #Store the result of the simulated annealing 
+resultcost = bestcostsofar
 
-newsolution, newcost = generateneighborsolution(solution)
-print(solution)
-print(newcost)
-print(newsolution)
  
 #region print
-print("Total costs: {} at time {}.".format(warehouse.total_costs, warehouse.t))
-print(np.sum(costs))
+print(resultsolution)  
+print(resultcost)
+#print("Total costs: {} at time {}.".format(warehouse.total_costs, warehouse.t))
+#print(np.sum(costs))
 # Save solution to a JSON file.
 utils.create_missing_directories_of_file(SOLUTION_FILE)
 with open(SOLUTION_FILE, 'w') as outfile:
     recorder.store_solution_to_json(solution, outfile)
+progress_bar.close()
 #endregion
